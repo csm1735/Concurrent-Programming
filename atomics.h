bb10ddeebff95b692a46e378158fc83a352558b4
csm1735 <asd148148@gmail.com>
2023-06-19 20:34:21 +0800

    Rewrite the original code using C11 Atomics
    The original code was designed for x86(-64) implementation,
    so it has been modified using C11 Atomics to support
    other processor architectures.
    
    The current implementation still fails to pass Thread Sanitizer's
    runtime detection.

---
+++ atomics.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <stdatomic.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#define ATOMIC_SET atomic_flag_test_and_set
+#define ATOMIC_RELEASE atomic_flag_clear
+
+#define ATOMIC_SUB atomic_fetch_sub
+#define ATOMIC_SUB64 ATOMIC_SUB
+#define CAS atomic_compare_exchange_strong
+#define cmpxchg CAS
+/* The 2nd argument is limited to 1 on machines with TAS but not XCHG.
+ * On x86 it is an arbitrary value.
+ */
+#define XCHG atomic_exchange
+
+#define ATOMIC_ADD atomic_fetch_add
+#define ATOMIC_ADD64 ATOMIC_ADD
+#define mb() atomic_thread_fence(memory_order_seq_cst)
+
+/* compiler barrier only: runtime reordering is impossible on x86 */
+#define lmb() atomic_thread_fence(memory_order_seq_cst)
+#define smb() atomic_thread_fence(memory_order_seq_cst)
